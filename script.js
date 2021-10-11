8b5bef5c3dc670434c870de25d709a3702fee6b0
bidyutmaji <bidyutmaji12345@gmail.com>
2021-10-11 10:34:43 +0530

    Radhe Radhe

---
+++ script.js
@@ -0,0 +1,12 @@
+var clicks = 0;
+
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+document.addEventListener('volumeupbutton', () => {
+    clicks += 1;
+document.getElementById("clicks").innerHTML = clicks;
+    //Do something here
+  }, false);
